cda2e6d8ab62a14b3ffc91938221529c61056966
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-10 21:39:02 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -2,8 +2,8 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print("")
-print("""Welcome To MAFIA USERBOT String Session\nGenerator By @H1M4N5HU0P\n\n""")
-print("""Special Thanks To My Buddy Respective HellBot\nOwner @Kraken_The_BadASS\n\n""")
+print("""Welcome To Deadly USERBOT String Session\nGenerator By @SAMEER_795\n\n""")
+print("""LETS GET STARTED\n\n""")
 print("""Enter Your Valid Details To Continue!\n\n """)
 
 API_KEY = input("API_ID:  ")
@@ -19,11 +19,11 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @MAFIA_USERBOT For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @DEADLY_USERBOT For Any Help!\n\n",
             )
 
             print(
-                "Thanks for Choosing Mafia Userbot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing DEADLY Userbot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
